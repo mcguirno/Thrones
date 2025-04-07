c51b361aa23a011734df667591d015bf57d63041
nbmcguire <57973784+nbmcguire@users.noreply.github.com>
2025-04-07 17:04:07 -0400

    Initial Commit

---
+++ Thrones/ThronesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThronesApp.swift
+//  Thrones
+//
+//  Created by Noah McGuire on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ThronesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
